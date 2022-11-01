7e1d9248e2caa1e566489482a801e74d017978f0
Ahmed9711 <ahmedzaitoon40@gmail.com>
2022-11-01 14:19:08 +0200

    Project Structue setup with (Project-tasks) models

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,9 @@
 Rails.application.routes.draw do
+  resources :projects do
+    resources :tasks
+  end
   devise_for :users
-  root 'welcome#home'
+  root 'projects#index'
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
