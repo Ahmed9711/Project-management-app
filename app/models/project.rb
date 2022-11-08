817d0ff01af896695640ec7c4ce79ec685923625
Ahmed9711 <ahmedzaitoon40@gmail.com>
2022-11-08 16:26:53 +0200

    Project percentage completion

--- app/models/project.rb
+++ app/models/project.rb
@@ -1,4 +1,35 @@
 class Project < ApplicationRecord
   belongs_to :user
   has_many :tasks
+
+  def badge_color
+    case status
+    when 'not-started'
+      'secondary'
+    when 'in-progress'
+      'info'
+    else
+      'success'
+    end 
+  end
+
+  def status
+    return 'not-started' if tasks.none?
+
+    if tasks.all? { |task| task.complete? }
+      'complete'
+    elsif tasks.any? { |task| task.in_progress? || task.complete? }
+      'in-progress'
+    else
+      'not-started'
+    end
+  end
+
+  def percent_complete
+    return 0 if tasks.none?
+
+    complete_tasks = tasks.select { |task| task.complete? }.count
+    ((complete_tasks.to_f / tasks.count)*100).round
+  end
+
 end
